d07cac5a8d171232a69a9cba8e40138226b13f7e
Tushar Kumar <followtushar@gmai.com>
2017-06-29 13:22:25 +0530

    error resolving

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var connector = new builder.ChatConnector({
 });
 
 // Listen for messages from users 
-server.post('/api/messages', connector.listen());
+server.post('http://visionrobot.azurewebsites.net/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
